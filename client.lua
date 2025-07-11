a95e48995ba9a7571db9acf7304f10c34a606489
KmeNix <55399775+KmeNix@users.noreply.github.com>
2025-07-11 18:34:56 +0200

    update

--- client.lua
+++ client.lua
@@ -1,3 +1,5 @@
+local checkDistance = 2.0
+
 CreateThread(function()
     while true do
         Wait(0)
@@ -16,25 +18,25 @@ CreateThread(function()
                 local door2 = vehicleData[3]
 
                 if model == vehicleName then
-
-                    local rearOffset = GetOffsetFromEntityInWorldCoords(vehicle, 0.0, -2.0, 0.0)
+                    local rearOffset = GetOffsetFromEntityInWorldCoords(vehicle, 0.0, 0.0, 0.0)
                     local distance = #(playerCoords - rearOffset)
+                    if distance <= checkDistance then
+                        BeginTextCommandDisplayHelp('STRING')
+                        AddTextComponentSubstringPlayerName("Naciśnij ~INPUT_CONTEXT~ aby się przebrać!")
+                        EndTextCommandDisplayHelp(0, false, true, 0)
 
-                    BeginTextCommandDisplayHelp('STRING')
-                    AddTextComponentSubstringPlayerName("Naciśnij ~INPUT_CONTEXT~ aby się przebrać!")
-                    EndTextCommandDisplayHelp(0, false, true, 0)
-
-                    if IsControlJustPressed(0, 38) then -- E
-                        if GetVehicleDoorAngleRatio(vehicle, door1) > 0.1 or GetVehicleDoorAngleRatio(vehicle, door2) > 0.1 then
-                            if door1 ~= nil then SetVehicleDoorShut(vehicle, door1, false) end
-                            if door2 ~= nil then SetVehicleDoorShut(vehicle, door2, false) end
-                        else
-                            if door1 ~= nil then SetVehicleDoorOpen(vehicle, door1, false, false) end
-                            if door2 ~= nil then SetVehicleDoorOpen(vehicle, door2, false, false) end
-                            openClothesMenu(function()
+                        if IsControlJustPressed(0, 38) then -- E
+                            if GetVehicleDoorAngleRatio(vehicle, door1) > 0.1 or GetVehicleDoorAngleRatio(vehicle, door2) > 0.1 then
                                 if door1 ~= nil then SetVehicleDoorShut(vehicle, door1, false) end
                                 if door2 ~= nil then SetVehicleDoorShut(vehicle, door2, false) end
-                            end)
+                            else
+                                if door1 ~= nil then SetVehicleDoorOpen(vehicle, door1, false, false) end
+                                if door2 ~= nil then SetVehicleDoorOpen(vehicle, door2, false, false) end
+                                openClothesMenu(function()
+                                    if door1 ~= nil then SetVehicleDoorShut(vehicle, door1, false) end
+                                    if door2 ~= nil then SetVehicleDoorShut(vehicle, door2, false) end
+                                end)
+                            end
                         end
                     end
                 end
